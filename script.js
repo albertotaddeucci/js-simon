f019642f9141b400b715356f929c4c43c23975d4
alberto <alberto.taddeucci@gmail.com>
2024-02-09 14:19:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/**
+
+Sfruttiamo le timing functions 
+per fare il conto alla rovescia per la correzione di domani!
+Ogni secondo il nostro countdown dovrà scalare
+ fino alle 9:30 di lunedì mattina!
+
+*/
+
+
+/*
+
+Conto all arovesce fino alle 9 di domani
+ (da cambiare poi con il giorno esatto)
+ - prendo l'ora di adesso
+ - faccio (24+9)-ora =>totale ore da contare
+ - ad ogni ora aggiorno il counter
+
+
+*/
