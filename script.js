d23a3d748c9d83954f83008cd41c3c229a405837
alberto <alberto.taddeucci@gmail.com>
2024-02-09 18:43:09 +0100

    risolto delay iniziale

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ Ogni secondo il nostro countdown dovrà scalare
  fino alle 9:30 di lunedì mattina!
 
 */
+const expiringTime = new Date("2024-02-12T09:30:00");
+let tot = Date.parse(expiringTime) - Date.parse(new Date());
+let sec = Math.floor( (tot/1000) % 60 );
+let min = Math.floor( (tot/1000/60) % 60 ); 
+let hour = Math.floor( (tot/(1000*60*60)) % 24 );
+let days = Math.floor( tot/(1000*60*60*24) );
 
+document.getElementById("giorni").innerText = sec;
 
-
+document.getElementById("giorni").innerText = days;
+document.getElementById("ore").innerText = hour;
+document.getElementById("minuti").innerText = min;
+document.getElementById("secondi").innerText = sec;
 
 
 let end = document.getElementById("result")
@@ -45,51 +55,51 @@ function showTime(){
 
 
 
-/* -------------------------------- funzioni -------------------------------- */
-let hourEnd
-let minEnd
-function counterHour() {
-    hCounter++
-    let differenceHours = remainingHours - hCounter
-    hour.innerText = `ore mancanti: ${differenceHours}`
+// /* -------------------------------- tentativo con altro metodo -------------------------------- */
+// let hourEnd
+// let minEnd
+// function counterHour() {
+//     hCounter++
+//     let differenceHours = remainingHours - hCounter
+//     hour.innerText = `ore mancanti: ${differenceHours}`
 
-    if (hCounter >= remainingHours){
-        clearTimeout(myHour)
-        hourEnd = true
+//     if (hCounter >= remainingHours){
+//         clearTimeout(myHour)
+//         hourEnd = true
 
-    }
+//     }
     
-}
+// }
 
-function counterMinutes() {
-    minCounter++
-    let differenceMin = remainingMinutes - minCounter
-    minutes.innerText = `minuti mancanti: ${differenceMin}`
-
-
-    if (differenceMin===0){
-
-        minCounter = 0;   
-        minEnd = true     
-        // clearTimeout(myMinutes)
+// function counterMinutes() {
+//     minCounter++
+//     let differenceMin = remainingMinutes - minCounter
+//     minutes.innerText = `minuti mancanti: ${differenceMin}`
 
 
-    }
+//     if (differenceMin===0){
+
+//         minCounter = 0;   
+//         minEnd = true     
+//         // clearTimeout(myMinutes)
+
+
+//     }
 
     
-}
+// }
 
-function counterSeconds() {
-    secCounter++
-    let differenceSec = remainingSeconds - secCounter
-    seconds.innerText = `Secondi mancanti: ${differenceSec}`
+// function counterSeconds() {
+//     secCounter++
+//     let differenceSec = remainingSeconds - secCounter
+//     seconds.innerText = `Secondi mancanti: ${differenceSec}`
 
 
-    if (secCounter >= remainingSeconds){
+//     if (secCounter >= remainingSeconds){
 
-        secCounter = 0;    
-        let secEnd = true    
+//         secCounter = 0;    
+//         let secEnd = true    
 
-    }
+//     }
     
-}
+// }
